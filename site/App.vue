a1d540738f885f93762376d708568656cbd48fd7
Hsiaoming Yang <me@lepture.com>
2016-11-13 18:35:50 +0900

    More on todo list

--- site/App.vue
+++ site/App.vue
@@ -8,21 +8,37 @@
   </header>
   <aside class="du-layout-side">
     <div class="du-scroll">
-      <du-menu title="Navigation">
-        <du-menu-item icon="fa fa-television" to="/">Overview</du-menu-item>
+      <du-menu title="Dashboard">
+        <du-menu-item icon="fa fa-home" to="/">Home</du-menu-item>
+        <du-menu-item icon="fa fa-line-chart" to="/stats/">Stats</du-menu-item>
+      </du-menu>
+      <du-menu title="Documentation">
+        <du-menu-item icon="fa fa-television" to="/guide/">Guide</du-menu-item>
         <du-menu-tree icon="fa fa-th-large">
           <span slot="title">Basic</span>
-          <du-menu-item icon="fa fa-circle-o" to="/grid/">Grid</du-menu-item>
-          <du-menu-item icon="fa fa-circle-o" to="/colors/">Colors</du-menu-item>
+          <du-menu-item to="/grid/">Grid</du-menu-item>
+          <du-menu-item to="/colors/">Colors</du-menu-item>
+        </du-menu-tree>
+        <du-menu-tree icon="fa fa-bell-o">
+          <span slot="title">Message</span>
+          <du-menu-item to="/dialog/">Dialog</du-menu-item>
+          <du-menu-item to="/message/">Message</du-menu-item>
+        </du-menu-tree>
+        <du-menu-tree icon="fa fa-location-arrow">
+          <span slot="title">Navigation</span>
+          <du-menu-item to="/breadcrumb/">Breadcrumb</du-menu-item>
+          <du-menu-item to="/navigation/">Navigation</du-menu-item>
+          <du-menu-item to="/pagination/">Pagination</du-menu-item>
+          <du-menu-item to="/step/">Step</du-menu-item>
         </du-menu-tree>
         <du-menu-tree icon="fa fa-check-square-o">
           <span slot="title">Form</span>
-          <du-menu-item icon="fa fa-circle-o" to="/select/">Input</du-menu-item>
-          <du-menu-item icon="fa fa-circle-o" to="/select/">Radio</du-menu-item>
-          <du-menu-item icon="fa fa-circle-o" to="/select/">Checkbox</du-menu-item>
-          <du-menu-item icon="fa fa-circle-o" to="/select/">Slider</du-menu-item>
-          <du-menu-item icon="fa fa-circle-o" to="/select/">Select</du-menu-item>
-          <du-menu-item icon="fa fa-circle-o" to="/select/">Autocomplete</du-menu-item>
+          <du-menu-item to="/input/">Input</du-menu-item>
+          <du-menu-item to="/radio/">Radio</du-menu-item>
+          <du-menu-item to="/checkbox/">Checkbox</du-menu-item>
+          <du-menu-item to="/slider/">Slider</du-menu-item>
+          <du-menu-item to="/select/">Select</du-menu-item>
+          <du-menu-item to="/autocomplete/">Autocomplete</du-menu-item>
         </du-menu-tree>
         <du-menu-tree icon="fa fa-calendar">
           <span slot="title">Time</span>
@@ -30,10 +46,10 @@
         </du-menu-tree>
         <du-menu-tree icon="fa fa-pie-chart">
           <span slot="title">Chart</span>
-          <du-menu-item icon="fa fa-circle-o" to="/">Line</du-menu-item>
-          <du-menu-item icon="fa fa-circle-o" to="/">Area</du-menu-item>
-          <du-menu-item icon="fa fa-circle-o" to="/">Bar</du-menu-item>
-          <du-menu-item icon="fa fa-circle-o" to="/">Pie</du-menu-item>
+          <du-menu-item to="/line-chart/">Line</du-menu-item>
+          <du-menu-item to="/area-chart/">Area</du-menu-item>
+          <du-menu-item to="/bar-chart/">Bar</du-menu-item>
+          <du-menu-item to="/pie-chart/">Pie</du-menu-item>
         </du-menu-tree>
       </du-menu>
     </div>
