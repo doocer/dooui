e534d99df015052276991c65bdeaddc1776cc1fd
Hsiaoming Yang <me@lepture.com>
2016-11-27 21:04:29 +0900

    Remove box vue file

--- components/box/index.js
+++ components/box/index.js
@@ -1,4 +1,25 @@
-import Box from './Box.vue'
+const Box = {
+  name: 'du-box',
+  render(h) {
+    var children = []
+    var heads = this.$slots.head
+    if (heads) {
+      children.push(
+        h('div', {'class': 'du-box_head'}, heads)
+      )
+    }
+    children.push(
+      h('div', {'class': 'du-box_body'}, this.$slots.default)
+    )
+    var foots = this.$slots.foot
+    if (foots) {
+      children.push(
+        h('div', {'class': 'du-box_foot'}), foots
+      )
+    }
+    return h('div', {'class': 'du-box'}, children)
+  }
+}
 
 function install(Vue) {
   Vue.component(Box.name, Box)
