e239d89638e1fa22e9a4dc2140936c8167d234ba
Hsiaoming Yang <me@lepture.com>
2016-11-28 13:16:35 +0900

    More props on pagination

--- components/pagination/Pagination.vue
+++ components/pagination/Pagination.vue
@@ -10,6 +10,22 @@ export default {
       type: Number,
       required: true
     },
+    leftEdge: {
+      type: Number,
+      default: 1
+    },
+    leftCurrent: {
+      type: Number,
+      default: 2
+    },
+    rightEdge: {
+      type: Number,
+      default: 1
+    },
+    rightCurrent: {
+      type: Number,
+      default: 4
+    },
     perpage: {
       type: Number,
       required: true,
@@ -22,13 +38,23 @@ export default {
   computed: {
     pages() {
       var count = parseInt((this.total - 1) / this.perpage, 10) + 1
-      var i = 0
+      var i = 0, last = 0
       var rv = []
       while (i <= count) {
-        rv.push(i)
+        if (
+          i <= this.leftEdge ||
+          (
+            i > this.current - this.leftCurrent - 1 &&
+            i < this.current + this.rightCurrent
+          ) ||
+          i > count - this.right_edge
+        ) {
+          rv.push(i)
+          last = i
+        }
       }
       return rv
     }
-  }
+  },
 }
 </script>
