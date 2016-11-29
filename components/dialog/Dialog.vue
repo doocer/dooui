3251967e75c25995328bb721967a59ad1af2ff8a
Hsiaoming Yang <me@lepture.com>
2016-11-29 15:47:49 +0900

    Add dialog documentation

--- components/dialog/Dialog.vue
+++ components/dialog/Dialog.vue
@@ -57,7 +57,7 @@ export default {
   },
   data() {
     return {
-      visible: true
+      visible: false
     }
   },
   computed: {
--- site/message/Dialog.vue
+++ site/message/Dialog.vue
@@ -3,5 +3,25 @@
   <demo file="dialog-0.vue">
     <h2>Dialog</h2>
   </demo>
+  <doc-attrs name="du-dialog" :descriptions="attrDocs"></doc-attrs>
 </div>
 </template>
+
+<script>
+const attrDocs = [
+  ['mask', 'transparent black mask below dialog'],
+  ['top', 'top offset of the dialog'],
+  ['width', 'dialog box width'],
+  ['title', 'title of the dialog'],
+  ['confirmText', 'text for the confirm button'],
+  ['cancelText', 'text for the cancle button'],
+]
+
+export default {
+  data() {
+    return {
+      attrDocs: attrDocs,
+    }
+  }
+}
+</script>
