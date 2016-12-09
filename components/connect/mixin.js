5ce978794c36b5af459f50cac4d63f107edcaada
Hsiaoming Yang <me@lepture.com>
2016-12-09 16:48:54 +0900

    Popover works

--- components/connect/mixin.js
+++ components/connect/mixin.js
@@ -1,8 +1,19 @@
 var Z_INDEX = 1000
-import out from 'vue-document-event'
+var ID_COUNT = 0
+var VM_MAP = {}
+
+listen(document, 'click', (e) => {
+  Object.keys(VM_MAP).forEach(ident => {
+    var vm = VM_MAP[ident]
+    if (vm.$refs.pop.contains(e.target) || vm.refEl.contains(e.target)) {
+      return
+    }
+    vm.visible = false
+  })
+})
 
 export default {
-  directives: {out},
+  gap: 0,
   props: {
     placement: {
       type: String,
@@ -15,14 +26,13 @@ export default {
   },
   data() {
     return {
+      _id: ID_COUNT++,
+      _inserted: false,
+      altPlacement: this.placement,
       refEl: null,
       visible: false,
-      position: {},
+      style: {},
       zIndex: Z_INDEX
-    }
-  },
-  computed: {
-    style() {
     }
   },
   methods: {
@@ -35,24 +45,107 @@ export default {
       const refRect = this.refEl.getBoundingClientRect()
       const top = refRect.top + (window.pageYOffset || body.scrollTop)
       const left = refRect.left + (window.pageXOffset || body.scrollLeft)
-      const right = body.clientWidth - refRect.right
+      const right = left + refRect.width
+      const bottom = top + refRect.height
 
-      const elRect = this.$el.getBoundingClientRect()
-      const viewTop = refRect.top >= elRect.height
-      const viewBottom = (vh - refRect.bottom) >= elRect.height
-      const viewLeft = refRect.left >= elRect.width
-      const viewRight = (vw - refRect.right) >= elRect.width
+      const popRect = this.$refs.pop.getBoundingClientRect()
+
+      var placement = this.placement.toLowerCase()
+      const gap = this.$options.gap
+
+      const vStyle = (placement) => {
+        var rv = {}
+        if (/left$/.test(placement)) {
+          rv.left = left
+        } else if (/right$/.test(placement)) {
+          rv.left = right - refRect.width
+        } else {
+          rv.left = left - (popRect.width - refRect.width) / 2
+        }
+        rv.left += 'px'
+        if (/^top/.test(placement)) {
+          rv.top = top - popRect.height - gap
+        } else {
+          rv.top = bottom + gap
+        }
+        rv.top += 'px'
+        this.style = rv
+      }
+      const hStyle = (placement) => {
+        var rv = {}
+        if (/top$/.test(placement)) {
+          rv.top = top
+        } else if (/bottom$/.test(placement)) {
+          rv.top = top + refRect.height - popRect.height
+        } else {
+          rv.top = top + (refRect.height - popRect.height) / 2
+        }
+        rv.top += 'px'
+        if (/^left/.test(placement)) {
+          rv.left = left - popRect.width - gap
+        } else {
+          rv.left = right + gap
+        }
+        rv.left += 'px'
+        this.style = rv
+      }
+
+      if (/^top/.test(placement)) {
+        if (
+          refRect.top < (popRect.height + gap) &&
+          (vh - refRect.bottom) >= (popRect.height + gap)
+        ) {
+          placement = placement.replace(/^top/, 'bottom')
+        }
+        vStyle(placement)
+      } else if (/^bottom/.test(placement)) {
+        if (
+          (vh - refRect.bottom) < (popRect.height + gap) &&
+          refRect.top >= (popRect.height + gap)
+        ) {
+          placement = placement.replace(/^bottom/, 'top')
+        }
+        vStyle(placement)
+      } else if (/^left/.test(placement)) {
+        if (
+          refRect.left < (popRect.width + gap) &&
+          (vw - refRect.right) >= (popRect.width + gap)
+        ) {
+          placement = placement.replace(/^left/, 'right')
+        }
+        hStyle(placement)
+      } else if (/^right/.test(placement)) {
+        if (
+          (vw - refRect.right) < (popRect.width + gap) &&
+          refRect.left >= (popRect.width + gap)
+        ) {
+          placement = placement.replace(/^right/, 'left')
+        }
+        hStyle(placement)
+      }
+      this.altPlacement = placement
     },
     show() {
-      if (this.refEl) {
-        this.updatePosition()
+      if (!this._inserted) {
+        document.body.appendChild(this.$refs.pop)
+        this._inserted = true
       }
       Z_INDEX += 1
       this.zIndex = Z_INDEX
-      this.visible = 'true'
+      this.visible = true
+      if (this.refEl) {
+        this.$nextTick(this.updatePosition)
+      }
     },
     hide() {
       this.visible = false
+    },
+    toggle() {
+      if (this.visible) {
+        this.hide()
+      } else {
+        this.show()
+      }
     },
     delayHide() {
       setTimeout(() => {
@@ -61,19 +154,22 @@ export default {
     },
     connect(el, vnode) {
       this.refEl = el
-      var vm = this
+      VM_MAP[this._id] = this
       if (this.trigger === 'click') {
-        listen(el, 'click', this.show)
+        listen(el, 'click', this.toggle)
       }
     },
   },
-  destroyed() {
-    if (this.refEl) {
-      unlisten(this.refEl, 'click', this.show)
+  beforeDestroy() {
+    if (this.$refs.pop && this.$refs.pop.parentNode === document.body) {
+      document.body.removeChild(this.$refs.pop)
     }
   },
-  mounted() {
-    // document.body.appendChild(this.$el)
+  destroyed() {
+    delete VM_MAP[this._id]
+    if (this.refEl) {
+      unlisten(this.refEl, 'click', this.toggle)
+    }
   }
 }
 
--- components/popover/Popover.vue
+++ components/popover/Popover.vue
@@ -1,10 +1,16 @@
 <template>
-<div class="du-popover" v-show="visible">
-  <div class="du-popover_title">
-    <slot name="title">{{title}}</slot>
-  </div>
-  <div class="du-popover_content">
-    <slot>{{content}}</slot>
+<div style="display:none">
+  <div class="du-popover" :class="altPlacement" :style="style"
+    v-show="visible" ref="pop">
+    <div class="du-popover_inner">
+      <div class="du-popover_title">
+        <slot name="title">{{title}}</slot>
+      </div>
+      <div class="du-popover_content">
+        <slot name="content">{{content}}</slot>
+      </div>
+    </div>
+    <span class="du-popover_arrow"></span>
   </div>
 </div>
 </template>
@@ -13,6 +19,7 @@
 import connectMixin from '../connect/mixin'
 export default {
   name: 'du-popover',
+  gap: 4,
   mixins: [connectMixin],
   props: {
     title: {
@@ -24,3 +31,22 @@ export default {
   }
 }
 </script>
+
+<style>
+.du-popover {
+  position: absolute;
+  min-width: 110px;
+}
+.du-popover_inner {
+  box-shadow: 0 1px 6px rgba(0, 0, 0, 0.2);
+  background-color: var(--color-white);
+  border-radius: 3px;
+}
+.du-popover_title {
+  padding: 0 16px;
+  line-height: 2;
+}
+.du-popover_content {
+  padding: 8px 16px;
+}
+</style>
