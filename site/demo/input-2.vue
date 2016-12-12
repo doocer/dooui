3d2ceac1fe69c9a178a3ae267525a8b0b818b959
Hsiaoming Yang <me@lepture.com>
2016-12-12 20:07:05 +0900

    Fix select in group input

---
+++ site/demo/input-2.vue
@@ -0,0 +1,9 @@
+<template>
+<du-input>
+  <du-select slot="prepend">
+    <du-option value="http://"></du-option>
+    <du-option value="https://"></du-option>
+  </du-select>
+  <span slot="append">.com</span>
+</du-input>
+</template>
--- site/pages/Input.vue
+++ site/pages/Input.vue
@@ -2,9 +2,6 @@
 <div class="view-input content">
   <demo file="input-0.vue">Input</demo>
   <demo file="input-1.vue">Input</demo>
-  <du-input icon="fa fa-clock-o" disabled>
-    <span slot="prepend">http://</span>
-    <span slot="append">.com</span>
-  </du-input>
+  <demo file="input-2.vue">Input</demo>
 </div>
 </template>
