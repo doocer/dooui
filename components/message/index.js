fb72dc667c9cfaf0666e3ce164bf8fbf4c72894a
Hsiaoming Yang <me@lepture.com>
2017-01-27 17:11:09 +0900

    You can use message out of vue instance

--- components/message/index.js
+++ components/message/index.js
@@ -1,47 +1,60 @@
 import MessageComponent from './Message.vue'
 
-function install(Vue) {
+let mounted = false
+let instance = null
+const options = {
+  top: 24,
+  duration: 4
+}
+
+
+function message(text, type, duration) {
+  if (!instance) {
+    throw new Error("Vue.use(Message) is required")
+  }
+
+  if (!mounted) {
+    document.body.appendChild(instance.$el)
+    mounted = true
+  }
+  type = type || 'info'
+  duration = duration || options.duration
+  return instance.flash(text, type, duration, options.top + 'px')
+}
+
+message.config = function(conf) {
+  Object.keys(conf).forEach(k => {
+    options[k] = conf[k]
+  })
+}
+
+message.info = function(text, duration) {
+  return message(text, 'info', duration)
+}
+
+message.success = function(text, duration) {
+  return message(text, 'success', duration)
+}
+
+message.warning = message.warn = function(text, duration) {
+  return message(text, 'warn', duration)
+}
+
+message.error = function(text, duration) {
+  return message(text, 'error', duration)
+}
+
+message.clear = function(id) {
+  if (!instance) {
+    return null
+  }
+  return instance.clear(id)
+}
+
+message.install = function(Vue) {
   const Message = Vue.extend(MessageComponent)
-  const instance = new Message()
+  instance = new Message()
   instance.$mount()
-  var mounted = false
-  var options = {
-    top: 24,
-    duration: 2.5
-  }
-
-  function message(text, type, duration) {
-    if (!mounted) {
-      document.body.appendChild(instance.$el)
-      mounted = true
-    }
-    type = type || 'info'
-    duration = duration || options.duration
-    return instance.flash(text, type, duration, options.top + 'px')
-  }
-
-  message.config = function(conf) {
-    Object.keys(conf).forEach(k => {
-      options[k] = conf[k]
-    })
-  }
-
-  message.info = function(text, duration) {
-    return message(text, 'info', duration)
-  }
-  message.success = function(text, duration) {
-    return message(text, 'success', duration)
-  }
-  message.warning = message.warn = function(text, duration) {
-    return message(text, 'warn', duration)
-  }
-  message.error = function(text, duration) {
-    return message(text, 'error', duration)
-  }
-  message.clear = function(id) {
-    return instance.clear(id)
-  }
-
   Vue.mixin({
     beforeCreate() {
       this.$message = message
@@ -49,4 +62,4 @@ function install(Vue) {
   })
 }
 
-export default {install}
+export default message
